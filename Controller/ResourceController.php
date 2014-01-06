1fd900d9a63d79d77edc4fe127eafb80ce0cfc14
Craig Blanchette <craig.blanchette@gmail.com>
2014-01-06 12:53:26 -0500

    Moved the ResourceExtension stuff to it's own small "bundle" so our other resources won't have to include the SymEditCore

---
+++ Controller/ResourceController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace SymEdit\Bundle\ResourceBundle\Controller;
+
+use Sylius\Bundle\ResourceBundle\Controller\ResourceController as BaseResourceController;
+
+class ResourceController extends BaseResourceController
+{
+
+}
